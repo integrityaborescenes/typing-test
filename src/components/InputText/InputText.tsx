93b211e2faac32a362c9726d3fbcbb436f0d682e
Vladislav Shitov <sayroxskrim@bk.ru>
2026-01-12 17:41:08 +0400

    добавил отоброжение текущей позиции набираемых букв

--- src/components/InputText/InputText.tsx
+++ src/components/InputText/InputText.tsx
@@ -9,25 +9,26 @@ const InputText = () => {
   let filteredText = text.slice(0, lastWord);
 
   const [value, setValue] = useState<string[]>([]);
+  // const [errors, setErrors] = useState<number>(0);
 
   const typingText = (e: ChangeEvent<HTMLTextAreaElement>) => {
     let currentValue = e.target.value;
     setValue(currentValue.split(""));
   };
 
-  console.log(value[value.length - 1]);
   return (
     <div className={styles.container}>
       <div className={styles.text}>
         {filteredText.map((t, i) => (
           <span
-            className={`${!value[i] ? "" : value[i] === t ? styles.true : styles.error}`}
+            key={i}
+            className={`${value[i - 1] && !value[i] ? styles.markCurrent : ""} ${!value[i] ? "" : value[i] !== t ? styles.error : styles.true}`}
           >
             {t}
           </span>
         ))}
       </div>
-      <textarea className={styles.textArea} onChange={typingText} />
+      <textarea className={styles.textArea} autoFocus onChange={typingText} />
     </div>
   );
 };
