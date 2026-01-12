9792be2c5927bf6b17f123f5df9f6b688d8172c4
Vladislav Shitov <sayroxskrim@bk.ru>
2026-01-12 17:03:49 +0400

    добавил визуальное отоброжение правильных и не правильных букв

--- src/components/InputText/InputText.tsx
+++ src/components/InputText/InputText.tsx
@@ -1,19 +1,33 @@
 import styles from "./InputText.module.scss";
 import { useGetTextQuery } from "../../store/services/text.api.ts";
+import { type ChangeEvent, useState } from "react";
 
 const InputText = () => {
   const { data } = useGetTextQuery();
   let text = data?.[0].toLowerCase().split("") || [];
   let lastWord = text.lastIndexOf(" ", 250);
   let filteredText = text.slice(0, lastWord);
+
+  const [value, setValue] = useState<string[]>([]);
+
+  const typingText = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    let currentValue = e.target.value;
+    setValue(currentValue.split(""));
+  };
+
+  console.log(value[value.length - 1]);
   return (
     <div className={styles.container}>
       <div className={styles.text}>
         {filteredText.map((t, i) => (
-          <span key={i}>{t}</span>
+          <span
+            className={`${!value[i] ? "" : value[i] === t ? styles.true : styles.error}`}
+          >
+            {t}
+          </span>
         ))}
       </div>
-      <textarea className={styles.textArea} />
+      <textarea className={styles.textArea} onChange={typingText} />
     </div>
   );
 };
