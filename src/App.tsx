9723e24454be3e3e0d5711727124fad6854f234c
Vladislav Shitov <sayroxskrim@bk.ru>
2026-01-14 17:08:47 +0400

    добавил спиннер пока фетчатся данные

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import InputText from "./components/InputText/InputText.tsx";
 import Keyboard from "./components/Keyboard/Keyboard.tsx";
 import Timer from "./components/Timer/Timer.tsx";
+import { useGetTextQuery } from "./store/services/text.api.ts";
+import Spinner from "./components/Spinner/Spinner.tsx";
 
 function App() {
+  const { isLoading } = useGetTextQuery();
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
   return (
     <>
       <Timer />
